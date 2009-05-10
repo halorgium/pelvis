04db07513a55dab9a523b9e57be2e2c51077087f
Tim Carey-Smith <tim@spork.in>
2009-05-11 05:11:41 +1200

    Namespace everything

---
+++ lib/pelvis.rb
@@ -0,0 +1,20 @@
+require 'eventmachine'
+require 'hmac-sha2'
+
+$:.unshift File.dirname(__FILE__)
+
+require 'pelvis/router'
+require 'pelvis/agent'
+require 'pelvis/job'
+
+require 'pelvis/outcall'
+require 'pelvis/evocation'
+
+require 'pelvis/incall'
+require 'pelvis/invocation'
+
+require 'logger'
+
+module Pelvis
+  LOGGER = Logger.new($stderr)
+end
---
+++ lib/pelvis/invocation.rb
@@ -0,0 +1,43 @@
+module Pelvis
+  class Invocation
+    include EM::Deferrable
+
+    def self.start(*args)
+      new(*args).start
+    end
+
+    def initialize(incall, operation)
+      @incall, @operation = incall, operation
+    end
+    attr_reader :incall, :operation
+
+    def start
+      LOGGER.debug "starting invocation: #{@operation.inspect}"
+      @operation.call(self)
+      self
+    end
+
+    def receive(data)
+      LOGGER.debug "received data from operation #{@operation}: #{data.inspect}"
+      @incall.receive(self, data)
+    end
+
+    def router
+      @incall.router
+    end
+
+    def job
+      @incall.job
+    end
+
+    def complete(data)
+      LOGGER.debug "completed operation #{@operation}: #{data.inspect}"
+      @complete = true
+      succeed(data)
+    end
+
+    def complete?
+      @complete
+    end
+  end
+end
