7387c017fbb5225f74a3dcb7149a04cd4150d152
Tim Carey-Smith <tim@spork.in>
2009-05-12 12:50:31 +1200

    Simplify the API for starting an agent

--- lib/pelvis.rb
+++ lib/pelvis.rb
@@ -4,7 +4,6 @@ require 'extlib'
 
 $:.unshift File.dirname(__FILE__)
 
-require 'pelvis/router'
 require 'pelvis/agent'
 require 'pelvis/job'
 require 'pelvis/actor'
@@ -25,4 +24,13 @@ require 'logger'
 
 module Pelvis
   LOGGER = Logger.new($stderr)
+
+  def self.connect(protocol_name, protocol_options, actors = nil, &block)
+    protocol = Protocols.connect(protocol_name, self, protocol_options, actors)
+    protocol.callback(&block) if block_given?
+    protocol.errback do |r|
+      LOGGER.error "Could not connect to protocol: #{protocol.inspect}, #{r.inspect}"
+    end
+    protocol
+  end
 end
