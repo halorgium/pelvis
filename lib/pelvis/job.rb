12a9f99c2d41c50d5718dee2b032f9f0cd482f82
lee <lee@vertebra-test1.localdomain>
2009-05-13 14:09:37 -0600

    adding option to delegate to an object passed as callback

--- lib/pelvis/job.rb
+++ lib/pelvis/job.rb
@@ -8,7 +8,8 @@ module Pelvis
       if block_given?
         klass = Class.new(Job, &block)
       end
-      if mod
+      if mod && mod.kind_of?(Module)
+        options.delete(:callback)
         klass = Class.new(Job) do
           include mod
         end
@@ -22,6 +23,8 @@ module Pelvis
       unless [:init, :direct, :all].include?(scope)
         raise ArgumentError, "Scope #{scope.inspect} is not valid"
       end
+
+      @delegate = options[:callback] if options[:callback]
       @token, @scope, @operation, @args, @options, @parent = token, scope, operation, args, options, parent
       @token << ":#{@parent.job.token}" if @parent
     end
@@ -29,14 +32,24 @@ module Pelvis
 
     def receive(data)
       LOGGER.debug "data: Doing nothing with #{data.inspect}"
+      delegate(:receive, data)
     end
 
     def complete(event)
       LOGGER.debug "complete: Doing nothing with #{event.inspect}"
+      delegate(:complete, event)
     end
 
     def error(event)
       LOGGER.debug "error: Doing nothing with #{event.inspect}"
+      delegate(:error, event)
     end
+
+    private
+      def delegate(method, *args)
+        if @delegate && @delegate.respond_to?(method)
+          @delegate.send(method, *args)
+        end
+      end
   end
 end
